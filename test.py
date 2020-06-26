72dc24d41748d686b241dda7e00890609bebe633
GibranBenitez <gibran@ieee.org>
2020-06-25 19:45:59 -0500

    version 2

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import numpy as np
 
 vector = np.array([123, 12, 1])
+variable = "lista"
 print('esta es una pruba: {}'.format(vector))
-print('segunda version')
+print('segunda version {}'.format(variable))
